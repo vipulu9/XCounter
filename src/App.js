792c6ddf7a55b9162b329a6363b92fcc8ace6cbf
vipulu9 <vipuluniyal9@gmail.com>
2025-05-08 20:04:33 +0530

    XCounter

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Counter from './Counter';
+
+const App = () => {
+  return (
+    <div className="App">
+      <h1>Counter App</h1>
+      <Counter />
+    </div>
+  );
+};
+
+export default App;
---
+++ src/Counter.js
@@ -0,0 +1,18 @@
+import React, { useState } from 'react';
+
+const Counter = () => {
+  const [count, setCount] = useState(0);
+
+  const increment = () => setCount(prevCount => prevCount + 1);
+  const decrement = () => setCount(prevCount => prevCount - 1);
+
+  return (
+    <div>
+      <h2>Count: {count}</h2>
+      <button onClick={increment}>Increment</button>
+      <button onClick={decrement}>Decrement</button>
+    </div>
+  );
+};
+
+export default Counter;
